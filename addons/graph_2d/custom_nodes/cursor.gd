9d6553f87cd3c656ba2b81a2ac1e332ef3a701a5
Jacques Duflos <31228096+JacquesDuflos@users.noreply.github.com>
2026-08-27 22:19:01 -0500

    coordinate next to mouse and curser extend correction

--- addons/graph_2d/custom_nodes/cursor.gd
+++ addons/graph_2d/custom_nodes/cursor.gd
@@ -2,20 +2,31 @@ extends Control
 class_name PlotCursor
 
 var graph : Graph2D
+## The x coordinate displayed next to the mouse
+var x_label : Label
+## The y coordinate displayed next to the mouse
+var y_label : Label
 
 
 func _ready() -> void:
 	graph = get_parent().get_parent() as Graph2D
+	x_label = Label.new()
+	x_label.rotation = PI/2
+	add_child(x_label)
+	y_label = Label.new()
+	add_child(y_label)
 
 
 func _draw() -> void:
-	draw_circle(graph.mouse_px, 20, Color.AQUA)
+	#draw_circle(graph.mouse_px, 20, Color.AQUA)
 	draw_horizontal_cursor()
 	draw_vertical_cursor()
 
 
 func draw_vertical_cursor():
-	if not graph.show_vertical_curser : return
+	if not graph.show_vertical_curser :
+		x_label.hide()
+		return
 	var fr : Vector2
 	var to : Vector2
 	match graph.vertical_curser_extend:
@@ -25,7 +36,12 @@ func draw_vertical_cursor():
 		1:
 			fr = graph.mouse_px
 			to = graph._coordinate_to_pixel(Vector2(0, graph.mouse_coor.y))
-	draw_line(fr, to, Color.OLD_LACE)
+			to.x = clamp(to.x, 0, graph.plot_area.size.x)
+	draw_line(fr, to, graph.y_color)
+	x_label.position = graph.mouse_px + Vector2(20,20)
+	x_label.modulate = graph.x_color
+	x_label.text = "%.3f" % [graph.mouse_coor.x]
+	x_label.show()
 
 
 func draw_horizontal_cursor():
@@ -39,5 +55,10 @@ func draw_horizontal_cursor():
 		1:
 			fr = graph.mouse_px
 			to = graph._coordinate_to_pixel(Vector2(graph.mouse_coor.x, 0))
-	draw_line(fr, to, Color.INDIAN_RED)
+			to.y = clamp(to.y, 0, graph.plot_area.size.y)
+	draw_line(fr, to, graph.x_color)
+	y_label.position = graph.mouse_px + Vector2(20,-20)
+	y_label.modulate = graph.y_color
+	y_label.text = "%.3f" % [graph.mouse_coor.y]
+	y_label.show()
 	
